28014af4f45a5e361b494cd8581e4021d7df67fb
Chizi Victor <chiziwokoma@gmail.com>
2025-09-09 23:32:56 +0100

    feat: superadmin only component

--- app/components/protected.tsx
+++ app/components/protected.tsx
@@ -1,49 +1,12 @@
 import { useLoggedInUser } from "@/hooks/data";
-import type { UserRole } from "@/lib/types";
 
-type ProtectedProps = {
+type SuperAdminOnlyProps = {
   children: React.ReactNode;
-} & ({ action: AppAction; roles?: never } | { roles: UserRole[]; action?: never });
-
-// TODO:
-// 1. This should be superadmin vs admin
-// 2. Move agent validation to new route group
-
-export function Protected({ children, action, roles }: ProtectedProps) {
-  const { data } = useLoggedInUser();
-  if (!data) return null;
-
-  console.log("User role", data.role, roles);
-  if (roles) {
-    if (roles.includes(data.role)) return children;
-
-    return null;
-  }
-
-  const permissions = PERMISSIONS_MAP[data.role] || [];
-  const hasPermission = permissions.includes(action);
-  if (hasPermission) return children;
-
-  return null;
-}
-
-type AppAction =
-  | "users:read"
-  | "users:delete"
-  | "users:create"
-  | "customers:read"
-  | "customers:delete"
-  | "customers:create"
-  | "branches:read";
-const PERMISSIONS_MAP: Record<UserRole, AppAction[]> = {
-  AGENT: ["branches:read", "users:read", "customers:read"],
-  ADMIN: [
-    "branches:read",
-    "users:read",
-    "users:delete",
-    "users:create",
-    "customers:read",
-    "customers:delete",
-    "customers:create",
-  ],
 };
+
+export function SuperAdminOnly({ children }: SuperAdminOnlyProps) {
+  const { data } = useLoggedInUser();
+  if (!data?.superAdmin) return null;
+
+  return children;
+}
